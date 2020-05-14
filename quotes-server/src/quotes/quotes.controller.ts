af6e44046f8dbcf82b5e8002e4ea8f9a7da5a330
Gabriel Albuquerque <gabriel.albuquerque@bit.com.br>
2020-05-14 09:39:54 -0300

    2-16

--- quotes-server/src/quotes/quotes.controller.ts
+++ quotes-server/src/quotes/quotes.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Body, Param } from '@nestjs/common';
 import { QuotesService } from 'src/services/quotes/quotes.service';
 import { CreateQuoteDto } from 'src/dto/create-quote.dto';
 import { ApiTags, ApiParam } from '@nestjs/swagger';
+import { Quote } from 'src/model/quotes.model';
 
 @ApiTags('quotes')
 @Controller('quotes')
@@ -10,19 +11,31 @@ export class QuotesController {
     constructor(private quotesService: QuotesService){}
 
     @Get()
-    getQuotes(): string {
+    getQuotes(): Quote[] {
         return this.quotesService.getQuotes();
     }
 
     @ApiParam({name: 'id'})
     @Get(':id')
-    getQuote(@Param('id') id) {
+    getQuote(@Param('id') id): Quote {
         return this.quotesService.getQuote(id)
     }
 
     @Post()
-    createQuotes(@Body() createQuoteDto: CreateQuoteDto): any  {
-      return this.quotesService.createQuotes(createQuoteDto);
+    createQuote(@Body() createQuoteDto: CreateQuoteDto): Quote  {
+      return this.quotesService.createQuote(createQuoteDto);
+    }
+
+    @ApiParam({name: 'id'})
+    @Put(':id')
+    updateQuote(@Param('id') id, @Body() updateQuoteDto: CreateQuoteDto): Quote  {
+      return this.quotesService.updateQuote(id, updateQuoteDto);
+    }
+
+    @ApiParam({name: 'id'})
+    @Delete(':id')
+    deleteQuote(@Param('id') id): Quote  {
+      return this.quotesService.deleteQuote(id);
     }
 
 }
--- quotes-server/src/services/quotes/quotes.service.ts
+++ quotes-server/src/services/quotes/quotes.service.ts
@@ -1,17 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { Quote } from 'src/model/quotes.model';
 
 @Injectable()
 export class QuotesService {
+    quotes: Quote[] = [
+        {
+           id: '1',
+           title: 'Titulo 1',
+           author: 'Gabriel'          
+        },
+        {
+            id: '2',
+            title: 'Titulo 2',
+            author: 'João'          
+         },
+         {
+            id: '3',
+            title: 'Titulo 3',
+            author: 'Cleusa'          
+         }
 
-    getQuotes(): string {
-        return 'Return LALA';
+    ]
+
+
+    getQuotes(): Quote[] {
+        return this.quotes;
     }
 
-    getQuote(id: number) {
-        return id;
+    getQuote(id: string): Quote {
+        return this.quotes.find(quote => quote.id === id);
     }
 
-    createQuotes(quote: any) {
+    createQuote(quote: Quote) {
         return quote;
+    }
+
+    updateQuote(id: string, updateQuoteDto): Quote {
+        let data = this.quotes.find(quote => quote.id === id);
+        data.title  = updateQuoteDto.title ? updateQuoteDto.title : data.title
+        data.author  = updateQuoteDto.author ? updateQuoteDto.author : data.author
+        return data;
+    }
+
+    deleteQuote(id: string){
+    return this.quotes.find(quote => quote.id === id);
     }
 }
